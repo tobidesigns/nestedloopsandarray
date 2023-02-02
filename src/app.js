cfd45606677f716b910fa7b60d7695297bcc9a37
Tobi <tobiraheem@gmail.com>
2023-02-02 19:04:43 +0000

    nested loops completed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "my"];
+let adj = ["great", "big", "massive"];
+let noun = ["jogger", "racoon", "lizard"];
+let domain = [".com", ".co.uk", ".net", ".org"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < domain.length; l++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);
+      }
+    }
+  }
+}
